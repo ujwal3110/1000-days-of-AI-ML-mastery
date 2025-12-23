eb38284aeadb04e278508713a5a6676680992f31
Ujwal Shanbhag <shanbhagujwal@gmail.com>
2025-12-23 20:17:12 +0530

    Create attention.py

---
+++ Phase 1 ( Day 1 - Day 100 )/foundation/math/micronumpy/nn/attention.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def scaled_dot_product_attention(Q, K, V):
+    d_k = Q.shape[-1]
+    scores = Q @ K.T / np.sqrt(d_k)
+
+    exp_scores = np.exp(scores - np.max(scores, axis=-1, keepdims=True))
+    weights = exp_scores / np.sum(exp_scores, axis=-1, keepdims=True)
+
+    return weights @ V
